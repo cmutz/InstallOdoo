e5cd142491ef5abad467dbc10c92f5c674180afa
cmutz <mutz.clement@gmail.com>
2019-11-05 01:17:37 +0100

    Update insert_module_oca_v10.py

--- insert_module_oca_v10.py
+++ insert_module_oca_v10.py
@@ -4,11 +4,11 @@ import os
 import sys                                                                                                                                                                                                         
 path = "branch_oca.ini"
 version_odoo = "10.0"
-path_sources = "/opt/odoo/odoo_v10/sources/"
+path_sources = "/opt/odoo/odoo_v10/sources/oca_addons"
 
 with open(path, "r") as f:
     contenu = f.read().splitlines()
     print(contenu)
     for liste in contenu:
         git_command = "https://github.com/OCA/" + liste
-        os.system("git clone " + git_command + " -b " + version_odoo + " " + path_sources + "/" + liste)  
+        os.system("git clone " + git_command + " -b " + version_odoo + " " + path_sources + "/" + liste)
